c8f85c3544384861966fa67424e5415f0d4cd950
sboursault <sboursault@protonmail.com>
2025-08-31 07:25:36 +0200

    add tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-//import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest'
+
+import { render } from 'vitest-browser-react'
 import App from './App';
 
 /*test('renders learn react link', () => {
@@ -7,3 +9,10 @@ import App from './App';
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });*/
+
+
+test('renders name', async () => {
+  const { getByText } = render(<App />)
+  await expect.element(getByText('Scenario Manager')).toBeInTheDocument()
+  //await getByRole('button', { name: 'Increment '}).click()
+})
